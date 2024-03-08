7d49ba3dc80ad9d39fcc710e1238e178c0a520c9
antonio99686 <antonio.2022324018@aluno.iffar.edu.br>
2024-03-08 09:37:10 -0300

    login

--- formcad.php
+++ formcad.php
@@ -5,8 +5,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/login_index.css">
-    <link rel="shortcut icon" href="img/logo.png">
+    <link rel="stylesheet" href="css/formcad.css">
+    <link rel="shortcut icon" href="img/cadastro.png">
     <title>sistema</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
@@ -19,7 +19,7 @@
                 <div class="col-lg-4 offset-lg-4">
                     <div class="card">
                         <div class="card-body">
-                            <h3>Cadastre-se</h3>
+                            <h3>Cadastro</h3>
                         </div>
                         <div class="card-body">
                             <form action="cadastrar.php" method="POST" enctype="multipart/form-data">
@@ -27,25 +27,25 @@
                         <div class="mb-3">
                             <label>Nome completo</label>
                             <input type="text" name="nome"
-                             class="form-control">
+                             class="form-control"  placeholder="nome">
                         </div>
                     </div>
                     <div>
                         <div class="mb-3">
                             <label>Senha</label>
                             <input type="password" name="senha" 
-                            class="form-control">
+                            class="form-control" placeholder="1234">
                         </div>
                     </div>
                     <div>
                         <div class="mb-3">
                             <label>E-mail</label>
                             <input type="email" name="email" 
-                            class="form-control">
+                            class="form-control " placeholder="example@gmail.com">
                         </div>
                     </div>
                    
-                       
+                    <p>Já tem conta?</p>    
                             <button type="submit" class="btn 
        btn-primary"> Enviar </button>
 
